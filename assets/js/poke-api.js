950841c3b92d5745bc128ec252bfc94425e0b770
joaocunha <jvscunha04@gmail.com>
2023-09-04 14:13:19 -0300

    feat: nova requisao para a pokeapi

--- assets/js/poke-api.js
+++ assets/js/poke-api.js
@@ -1,11 +1,20 @@
 const pokeApi = {}
 
-pokeApi.getPokemons = (offset = 0, limit = 10) => {
+pokeApi.getPokemonDetails = (pokemon) => {
+    return fetch(pokemon.url).then((response) => response.json())
+}
+
+pokeApi.getPokemons = (offset = 0, limit = 5) => {
 
     const url = `https://pokeapi.co/api/v2/pokemon?set=${offset}&limit=${limit}`;
 
     return fetch(url)
         .then((response) => response.json())
         .then((jsonBody) => jsonBody.results)
-        .catch((error) => console.error(error))
+        .then((Pokemons) => Pokemons.map(pokeApi.getPokemonDetails))
+        .then((detailRequests) => Promise.all(detailRequests))
+        .then((pokemonsDetails) => {
+            debugger
+            console.log(pokemonsDetails)
+        })
 }
