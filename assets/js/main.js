6c9576138f3cc4236a6ca0c727751b4226d8cc70
joaocunha <jvscunha04@gmail.com>
2023-09-03 21:21:57 -0300

    feat: incluindo um lista da api em html

--- assets/js/main.js
+++ assets/js/main.js
@@ -2,8 +2,37 @@ const offset = 0;
 const limit = 10;
 const url = `https://pokeapi.co/api/v2/pokemon?set=${offset}&limit=${limit}`;
 
+
+function convertPokemonToHTML(pokemon) {
+    return `
+    <li class="pokemon">
+    <span class="number"></span>
+    <span class="name">${pokemon.name}</span>
+
+    <div class="detail">
+        <ol class="types">
+            <li class="type"></li>
+            <li class="type"></li>
+        </ol>
+        <img src="${pokemon.name}">
+    </div>
+    </li>
+    `
+}
+
+const pokemonlist = document.getElementById('pokemonList');
+
 fetch(url)
     .then((response) => response.json())
-    .then((jsonBody) => console.log(jsonBody))
+    .then((jsonBody) => jsonBody.results)
+    .then((pokemons) => {
+
+        for (let i = 0; i < pokemons.length; i++) {
+            const pokemon = pokemons[i]
+            pokemonlist.innerHTML += convertPokemonToHTML(pokemon);
+
+
+        }
+    })
     .catch((error) => console.error(error))
 
