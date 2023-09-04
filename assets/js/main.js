c1028b89c71e6d3180b22ccc1d7a977d630a97e5
joaocunha <jvscunha04@gmail.com>
2023-09-04 13:22:41 -0300

    fix: corrgindo o arquivo poki.api

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,7 +1,3 @@
-const offset = 0;
-const limit = 10;
-const url = `https://pokeapi.co/api/v2/pokemon?set=${offset}&limit=${limit}`;
-
 
 function convertPokemonToHTML(pokemon) {
     return `
@@ -22,17 +18,7 @@ function convertPokemonToHTML(pokemon) {
 
 const pokemonlist = document.getElementById('pokemonList');
 
-fetch(url)
-    .then((response) => response.json())
-    .then((jsonBody) => jsonBody.results)
-    .then((pokemons) => {
 
-        for (let i = 0; i < pokemons.length; i++) {
-            const pokemon = pokemons[i]
-            pokemonlist.innerHTML += convertPokemonToHTML(pokemon);
-
-
-        }
-    })
-    .catch((error) => console.error(error))
-
+pokeApi.getPokemons().then((Pokemons = []) => {
+    pokemonlist.innerHTML += Pokemons.map(convertPokemonToHTML).join('')
+})
